7131ca29f3c3bb8ff80d1d588fdce590e056f3ee
Hackbright Student <info@hackbrightacademy.com>
2015-02-03 16:40:15 -0800

    Part 2 complete!  Added several new functions including handling for non-commands and a message on quit

--- hackbright.py
+++ hackbright.py
@@ -49,12 +49,43 @@ Description: %s
 Max Grade: %s""" % (title, description, max_grade)
 
 
+def get_student_grade(github, project_title):
+    query = """SELECT s.first_name, s.last_name, g.grade, p.max_grade
+                FROM Students AS s 
+                LEFT JOIN Grades AS g ON (s.github=g.student_github)
+                JOIN Projects AS p ON (g.project_title=p.title)
+                WHERE s.github = ? AND g.project_title= ?"""
+    DB.execute(query, (github, project_title))
+    row = DB.fetchone()
+    print """\
+    %s %s has a grade of %s out of a possible %s on that project.""" %(row[0], row[1], row[2], row[3])
+
+def give_student_grade(github,project,grade):
+    query = """INSERT INTO Grades
+                VALUES(?, ?, ?)"""
+    DB.execute(query, (github, project, grade))
+    CONN.commit()
+    print """\
+    Successfully assigned student with Github ID %s a grade of %s for the %s project."""%(github, grade, project)
+
+
+def show_all_grades(github):
+    query = """SELECT s.first_name, s.last_name, g.grade, g.project_title
+                FROM Students AS s
+                LEFT JOIN Grades AS g ON(s.github=g.student_github)
+                WHERE s.github = ?"""
+    DB.execute(query,(github,))
+    row = DB.fetchall()
+    for tuple in row:
+        print "%s %s has a grade of %s on the %s project." % (tuple[0], tuple[1], tuple[2], tuple[3])
+
+
 def main():
     connect_to_db()
     command = None
     while command != "quit":
         input_string = raw_input("HBA Database> ")
-        tokens = input_string.split("|")
+        tokens = input_string.split(",")
         command = tokens[0]
         args = tokens[1:]
 
@@ -66,7 +97,17 @@ def main():
             projects_by_title(*args)
         elif command == "new_project":
             add_a_project(*args)
-            
+        elif command == "project_grade":
+            get_student_grade(*args)
+        elif command == "give_grade":
+            give_student_grade(*args)
+        elif command =="show_grades":
+            show_all_grades(*args)
+        elif command == "quit":
+            print "Goodbye!"
+        else:
+            print "That command is not available -- please try again"
+
     CONN.close()
 
 if __name__ == "__main__":
